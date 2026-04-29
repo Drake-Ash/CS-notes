f802d83c967b37840d4669727753194c2a044fc1
Ash <ashwinv4444@gmail.com>
2026-04-29 23:07:24 +0530

    Create 1268-MarketAnalysisI

---
+++ leetcode/1268-MarketAnalysisI/1268-MarketAnalysisI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 29/04/2026, 23:07:24
+# Write your MySQL query statement below
+SELECT u.user_id AS buyer_id, join_date, COUNT(o.order_id) AS orders_in_2019
+FROM Users u
+LEFT JOIN Orders o ON u.user_id = o.buyer_id AND YEAR(order_date)='2019'
+GROUP BY u.user_id, u.join_date
