1f45c2324b3e0c98074167de13a2eb423ed178dd
Ash <ashwinv4444@gmail.com>
2026-04-29 23:07:19 +0530

    Create 1292-ImmediateFoodDeliveryIi

---
+++ leetcode/1292-ImmediateFoodDeliveryIi/1292-ImmediateFoodDeliveryIi.sql
@@ -0,0 +1,9 @@
+-- Last updated: 29/04/2026, 23:07:19
+Select 
+    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+)
