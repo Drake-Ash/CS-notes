af5cea02f7352ad90f844935ca36f17a9080c7f0
Ash <ashwinv4444@gmail.com>
2026-04-29 23:07:17 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ leetcode/1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.py
@@ -0,0 +1,7 @@
+# Last updated: 29/04/2026, 23:07:17
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        for i in range(len(arr) - 2):
+            if((arr[i] % 2 != 0) and (arr[i+1] % 2 != 0) and (arr[i+2] % 2 != 0)):
+                return True
+        return False
