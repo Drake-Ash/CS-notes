ff04651a34a2b9145a494c282097e278f479da69
Ash <ashwinv4444@gmail.com>
2026-04-29 23:03:47 +0530

    Create 2281-TheNumberOfPassengersInEachBusI

---
+++ leetcode/2281-TheNumberOfPassengersInEachBusI/2281-TheNumberOfPassengersInEachBusI.sql
@@ -0,0 +1,13 @@
+-- Last updated: 29/04/2026, 23:03:47
+with cte as 
+(select p.passenger_id,
+min(b.arrival_time) as b_arrival
+from Passengers p join Buses b on p.arrival_time <= b.arrival_time
+group by 1
+)
+
+select b.bus_id,
+count(passenger_id) as passengers_cnt
+from Buses b left join cte a on b.arrival_time = a.b_arrival
+group by 1
+order by 1 asc
