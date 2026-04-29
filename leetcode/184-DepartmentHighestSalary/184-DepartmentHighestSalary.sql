20ac37d48e7b7097ce9947d2b3f8272f8673ab00
Ash <ashwinv4444@gmail.com>
2026-04-29 23:11:49 +0530

    Create 184-DepartmentHighestSalary

---
+++ leetcode/184-DepartmentHighestSalary/184-DepartmentHighestSalary.sql
@@ -0,0 +1,9 @@
+-- Last updated: 29/04/2026, 23:11:49
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+JOIN Department d ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
