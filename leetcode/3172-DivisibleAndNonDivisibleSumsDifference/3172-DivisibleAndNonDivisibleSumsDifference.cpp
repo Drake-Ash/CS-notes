f6f8126c82696a96b435dbd6831b3917e90fbe20
Ash <ashwinv4444@gmail.com>
2026-04-29 23:02:27 +0530

    Create 3172-DivisibleAndNonDivisibleSumsDifference

---
+++ leetcode/3172-DivisibleAndNonDivisibleSumsDifference/3172-DivisibleAndNonDivisibleSumsDifference.cpp
@@ -0,0 +1,14 @@
+// Last updated: 29/04/2026, 23:02:27
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int nums1 = 0, nums2 = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i%m != 0) nums1 += i;
+            else nums2 += i;
+        }
+
+        return nums1 - nums2;
+    }
+};
