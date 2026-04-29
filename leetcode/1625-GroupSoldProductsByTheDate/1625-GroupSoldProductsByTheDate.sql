be0e64e177e5a67f367a584bf80c56d61e4c9d63
Ash <ashwinv4444@gmail.com>
2026-04-29 23:05:59 +0530

    Create 1625-GroupSoldProductsByTheDate

---
+++ leetcode/1625-GroupSoldProductsByTheDate/1625-GroupSoldProductsByTheDate.sql
@@ -0,0 +1,12 @@
+-- Last updated: 29/04/2026, 23:05:59
+# Write your MySQL query statement below
+select 
+    sell_date,
+    count(distinct product) as num_sold,
+    group_concat(distinct product order by product) as products
+from
+    Activities 
+group by 
+    sell_date
+order by
+    sell_date;
