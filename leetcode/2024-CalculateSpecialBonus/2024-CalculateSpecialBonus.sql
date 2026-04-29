3946b2dbdb351e2e6d0637a738f54ce87524c01b
Ash <ashwinv4444@gmail.com>
2026-04-29 23:04:32 +0530

    Create 2024-CalculateSpecialBonus

---
+++ leetcode/2024-CalculateSpecialBonus/2024-CalculateSpecialBonus.sql
@@ -0,0 +1,8 @@
+-- Last updated: 29/04/2026, 23:04:32
+   select employee_id , 
+   case when employee_id%2 <>0 and name not like 'M%' then salary 
+   else 0
+   end 
+   as bonus
+   from employees
+   order by employee_id;
