14ad508af409666bf38ab30c1aa36920a13dcef2
Ash <ashwinv4444@gmail.com>
2026-04-29 23:10:11 +0530

    Create 484-FindPermutation

---
+++ leetcode/484-FindPermutation/484-FindPermutation.py
@@ -0,0 +1,22 @@
+# Last updated: 29/04/2026, 23:10:11
+class Solution:
+    def findPermutation(self, s: str) -> List[int]:
+        # Initialize an empty stack and a result list
+        stack = []
+        res = []
+
+        # Append 'I' to the end of the string to handle the last part of the sequence
+        s += 'I'
+
+        # Iterate over each character in the modified string
+        for i in range(len(s)):
+            # Push the current index (1-based) onto the stack
+            stack.append(i + 1)
+
+            # When we encounter 'I', pop all elements from the stack and add them to the result
+            if s[i] == 'I':
+                while stack:
+                    res.append(stack.pop())
+
+        # Return the formed permutation
+        return res
