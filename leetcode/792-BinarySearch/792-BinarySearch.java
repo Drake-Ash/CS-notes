659eda756fa76e277f9c53cc2ff5fa991ae98579
Ash <ashwinv4444@gmail.com>
2026-04-29 23:09:00 +0530

    Create 792-BinarySearch

---
+++ leetcode/792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,17 @@
+// Last updated: 29/04/2026, 23:09:00
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0, r=nums.length-1;
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if(nums[mid]==target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+        return -1;
+    }
+}
