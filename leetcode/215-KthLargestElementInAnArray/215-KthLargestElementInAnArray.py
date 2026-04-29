ddd19fc42df6480283bc48b3250f1e61d00e7c78
Ash <ashwinv4444@gmail.com>
2026-04-29 23:11:27 +0530

    Create 215-KthLargestElementInAnArray

---
+++ leetcode/215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.py
@@ -0,0 +1,9 @@
+# Last updated: 29/04/2026, 23:11:26
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+
+        while len(nums) > k:
+            heapq.heappop(nums)
+        
+        return heapq.heappop(nums)
