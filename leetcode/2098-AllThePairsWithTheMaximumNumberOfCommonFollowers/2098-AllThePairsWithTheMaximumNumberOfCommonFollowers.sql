06b3808a01a410030609ac6ca50c6eb8c14e8c6f
Ash <ashwinv4444@gmail.com>
2026-04-29 23:04:17 +0530

    Create 2098-AllThePairsWithTheMaximumNumberOfCommonFollowers

---
+++ leetcode/2098-AllThePairsWithTheMaximumNumberOfCommonFollowers/2098-AllThePairsWithTheMaximumNumberOfCommonFollowers.sql
@@ -0,0 +1,15 @@
+-- Last updated: 29/04/2026, 23:04:17
+select user1_id, user2_id
+from
+(
+    select 
+    r1.user_id as user1_id, 
+    r2.user_id as user2_id, 
+    dense_rank()over(order by count(r1.follower_id) desc) as rk
+from Relations r1, Relations r2
+where 
+r1.user_id < r2.user_id 
+and r1.follower_id = r2.follower_id 
+group by r1.user_id, r2.user_id
+) temp
+where rk = 1
