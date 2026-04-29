eddd06122748eff194914dd25ec27d801d845ba6
Ash <ashwinv4444@gmail.com>
2026-04-29 23:13:29 +0530

    Create 50-PowxN

---
+++ leetcode/50-PowxN/50-PowxN.py
@@ -0,0 +1,14 @@
+# Last updated: 29/04/2026, 23:13:29
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if x == 0:
+                return 0
+            if n == 0:
+                return 1
+
+            res = helper(x * x, n // 2)
+            return x * res if n % 2 else res
+
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1 / res
