def0be94fdadeb0f4d6ab91de1fbce0129c7a3b5
Ash <ashwinv4444@gmail.com>
2026-04-29 23:02:56 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ leetcode/2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.ts
@@ -0,0 +1,12 @@
+// Last updated: 29/04/2026, 23:02:56
+function createHelloWorld() {
+    
+    return function(...args): string {
+        return "Hello World";
+    };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
