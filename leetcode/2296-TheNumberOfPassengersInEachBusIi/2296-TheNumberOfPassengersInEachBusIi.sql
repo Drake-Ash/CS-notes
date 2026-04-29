587c51c7a31a1b8ffd640e6f7be1f40cc8a66706
Ash <ashwinv4444@gmail.com>
2026-04-29 23:03:46 +0530

    Create 2296-TheNumberOfPassengersInEachBusIi

---
+++ leetcode/2296-TheNumberOfPassengersInEachBusIi/2296-TheNumberOfPassengersInEachBusIi.sql
@@ -0,0 +1,41 @@
+-- Last updated: 29/04/2026, 23:03:46
+# Write your MySQL query statement below
+
+WITH RECURSIVE cte AS (
+    SELECT 
+        b.bus_id,
+        b.capacity,
+        COUNT(p.passenger_id) AS p_count,
+        ROW_NUMBER() OVER(ORDER BY b.arrival_time ASC) AS rankin,
+        b.arrival_time
+    FROM Buses b
+    LEFT JOIN Passengers p ON b.arrival_time >= p.arrival_time
+    GROUP BY b.bus_id, b.capacity
+),
+cte2 AS (
+      SELECT 
+            bus_id,capacity,
+            IFNULL(p_count-(LAG(p_count) OVER()),p_count) AS diff, 
+            rankin
+      FROM cte
+),
+cte3 AS (
+## This is the most complex part - recursive ctes.
+      SELECT 
+            bus_id, 
+            LEAST(capacity,diff) AS boarded, 
+            GREATEST(0, diff-capacity) AS remain, 
+            rankin
+      FROM cte2
+      WHERE rankin = 1
+      UNION ALL
+      SELECT 
+            c2.bus_id, 
+            LEAST(capacity, diff+c3.remain) AS boarded, 
+            GREATEST(0, diff+c3.remain-capacity) AS remain, 
+            c2.rankin
+      FROM cte2 c2 JOIN cte3 c3
+      ON c2.rankin = c3.rankin+1
+)
+
+SELECT bus_id, boarded AS passengers_cnt FROM cte3 ORDER BY bus_id
