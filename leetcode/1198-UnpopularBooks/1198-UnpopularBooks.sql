4c73482f320c9455b5270ee5926e237cdc1782c3
Ash <ashwinv4444@gmail.com>
2026-04-29 23:07:43 +0530

    Create 1198-UnpopularBooks

---
+++ leetcode/1198-UnpopularBooks/1198-UnpopularBooks.sql
@@ -0,0 +1,11 @@
+-- Last updated: 29/04/2026, 23:07:43
+select b.book_id, b.name
+from books b left join
+    (select book_id, sum(quantity) as book_sold
+    from Orders 
+    where dispatch_date between '2018-06-23' and '2019-06-23'
+    group by book_id) t
+on b.book_id = t.book_id
+where available_from < '2019-05-23'
+and (book_sold is null or book_sold <10)
+order by b.book_id;
