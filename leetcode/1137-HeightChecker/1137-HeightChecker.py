03cccce58f75411bd55ea3da5019ce6444288d26
Ash <ashwinv4444@gmail.com>
2026-04-29 23:08:10 +0530

    Create 1137-HeightChecker

---
+++ leetcode/1137-HeightChecker/1137-HeightChecker.py
@@ -0,0 +1,8 @@
+# Last updated: 29/04/2026, 23:08:10
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        cnt = 0
+        for i,j in zip(heights,sorted(heights)):
+            if i != j:
+                cnt += 1
+        return cnt
