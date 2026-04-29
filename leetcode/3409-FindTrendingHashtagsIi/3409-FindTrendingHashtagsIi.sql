077f6a85f1f727aecc85357cdd7944bfd0a95549
Ash <ashwinv4444@gmail.com>
2026-04-29 23:01:43 +0530

    Create 3409-FindTrendingHashtagsIi

---
+++ leetcode/3409-FindTrendingHashtagsIi/3409-FindTrendingHashtagsIi.sql
@@ -0,0 +1,20 @@
+-- Last updated: 29/04/2026, 23:01:43
+# Write your MySQL query statement below
+WITH RECURSIVE tags AS (
+    SELECT REGEXP_SUBSTR (tweet, "#[^\\s]+") AS hashtag,
+           REGEXP_REPLACE(tweet, "#[^\\s]+", "", 1, 1) AS tweet
+    FROM Tweets
+    UNION ALL
+    SELECT REGEXP_SUBSTR (tweet, "#[^\\s]+") AS hashtag,
+           REGEXP_REPLACE(tweet, "#[^\\s]+", "", 1, 1) AS tweet
+    FROM tags
+    WHERE hashtag IS NOT NULL
+)
+
+SELECT hashtag,
+       COUNT(*) AS count
+FROM tags
+WHERE hashtag IS NOT NULL
+GROUP BY hashtag
+ORDER BY count DESC, hashtag DESC
+LIMIT 3
