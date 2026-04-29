5d39e770691954f9ed549ef7421ed0b1a9c07da1
Ash <ashwinv4444@gmail.com>
2026-04-29 23:07:58 +0530

    Create 1163-ProjectEmployeesIii

---
+++ leetcode/1163-ProjectEmployeesIii/1163-ProjectEmployeesIii.sql
@@ -0,0 +1,17 @@
+-- Last updated: 29/04/2026, 23:07:58
+# Write your MySQL query statement below
+with A 
+as
+(
+    select
+    p.project_id,
+    p.employee_id,
+    dense_rank() over (partition by p.project_id order by e.experience_years desc) as rnk
+    from
+    project p
+    join 
+    employee e
+    on p.employee_id = e.employee_id
+)
+
+select project_id,employee_id from A where rnk = 1
