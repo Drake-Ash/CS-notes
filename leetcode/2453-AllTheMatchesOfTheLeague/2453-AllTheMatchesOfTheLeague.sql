4f698ad09b571ce8afc11cb68e39646fc425b025
Ash <ashwinv4444@gmail.com>
2026-04-29 23:03:17 +0530

    Create 2453-AllTheMatchesOfTheLeague

---
+++ leetcode/2453-AllTheMatchesOfTheLeague/2453-AllTheMatchesOfTheLeague.sql
@@ -0,0 +1,10 @@
+-- Last updated: 29/04/2026, 23:03:17
+-- Write your PostgreSQL query statement below
+select
+t1.team_name as home_team,
+t2.team_name as away_team
+from
+teams t1
+join
+teams t2
+on t1.team_name <> t2.team_name
